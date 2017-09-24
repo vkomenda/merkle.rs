8f6c5fa806961a528f4e4a06d8039d84a8aac582
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-09-24 14:13:21 +0200

    Update documentation.

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["merkle"] = ["impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"merkle/struct.MerkleTree.html\" title=\"struct merkle::MerkleTree\">MerkleTree</a>&lt;T&gt;","impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"merkle/struct.Proof.html\" title=\"struct merkle::Proof\">Proof</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["merkle"] = ["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"merkle/struct.LeavesIterator.html\" title=\"struct merkle::LeavesIterator\">LeavesIterator</a>&lt;'a, T&gt;","impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"merkle/struct.LeavesIntoIterator.html\" title=\"struct merkle::LeavesIntoIterator\">LeavesIntoIterator</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
